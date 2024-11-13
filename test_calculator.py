6eb286e1bdcb56d198b0c8f4e2cc96d24ce1fa7a
Natchanon Phattamanuruk <oak0809@gmail.com>
2024-11-13 15:29:48 +0000

    add test case and fix calculator function

--- test_calculator.py
+++ test_calculator.py
@@ -9,7 +9,26 @@ class TestCalculator(unittest.TestCase):
     def test_add(self):
         self.assertEqual(self.calc.add(1, 2), 3)
 
+        self.assertEqual(self.calc.add(-1, 2), 1)
+        self.assertEqual(self.calc.add(-1, -1), -2)
     # Add the following test methods to the TestCalculator class:
+    def test_subtract(self):
+        self.assertEqual(self.calc.subtract(2, 1), 1)
+        self.assertEqual(self.calc.subtract(-1, 2), -3)
+
+    def test_multiply(self):
+        self.assertEqual(self.calc.multiply(2, 3), 6)
+        self.assertEqual(self.calc.multiply(-1, 2), -2)
+
+
+    def test_divide(self):
+        self.assertEqual(self.calc.divide(2, 1), 2)
+        self.assertEqual(self.calc.divide(0, 2), 0)
+
+    def test_modulo(self):
+        self.assertEqual(self.calc.modulo(2, 1), 0)
+        self.assertEqual(self.calc.modulo(9, 2), 1)
+     
 
 if __name__ == '__main__':
     unittest.main()
